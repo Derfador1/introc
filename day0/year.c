b791cac5150c9d4013e04ec9ce4517e1fa3122d7
Liam Echlin <wechlin@umbctraining.com>
2015-10-26 14:16:30 -0400

    Create example of if/then statements

---
+++ day0/year.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	int year = 1969;
+
+	if(year < 1984) {
+		printf("You are no friend of Big Brother\n");
+	} else if(year > 1999) {
+		printf("N'sync or Backstreet Boys?\n");
+	} else {
+		printf("Good Morning, Winston\n");
+		printf("Hammer or Vanilla Ice?\n");
+	}
+
+}
