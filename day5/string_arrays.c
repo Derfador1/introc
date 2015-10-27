80929561fa397ea14825fe972ed2d0943261bb7e
Liam Echlin <wechlin@umbctraining.com>
2015-10-27 12:23:14 -0400

    Remove useless code

--- day5/string_arrays.c
+++ day5/string_arrays.c
@@ -11,16 +11,8 @@ int main(void)
 	data[2] = "Arrowroot";
 	printf("%s\n", data[2]);
 
-	/*
-	data[3][8] = '9';
-	data[0][7] = 'i';
-	data[0][8] = '!';
-	*/
-
-
 	printf("%zd bytes\n", sizeof(data));
 	printf("%s\n", data[3]);
 	printf("%s\n", data[0]);
 	printf("%p\n", data[0]);
-
 }
