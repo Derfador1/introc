23680f3ec8db587a9dd9da8f530a5ae875b83eee
Liam Echlin <wechlin@umbctraining.com>
2015-10-28 10:44:23 -0400

    Change card struct to use suit lookup
    Add a array of strings to track suit names, and reference the suit by index
    lookup in the card struct.

--- day7/poker.c
+++ day7/poker.c
@@ -4,8 +4,10 @@
 
 struct card {
 	int rank;
-	char suit[9];
+	int suit_lookup;
 };
+
+char *suit_names[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
 void print_card(struct card);
 
@@ -13,13 +15,13 @@ int main(void)
 {
 	struct card three_of_hearts;
 
-	strncpy(three_of_hearts.suit, "Hearts", sizeof(three_of_hearts.suit));
 	three_of_hearts.rank = 3;
+	three_of_hearts.suit_lookup = 2;
 
 	print_card(three_of_hearts);
 }
 
 void print_card(struct card the_card)
 {
-	printf("%d of %s\n", the_card.rank, the_card.suit);
+	printf("%d of %s\n", the_card.rank, suit_names[the_card.suit_lookup]);
 }
