1df5b5355cd718bb7afc6dee74dfd43158b97d3a
Liam Echlin <wechlin@umbctraining.com>
2015-10-28 12:21:12 -0400

    Switch card structure to use pointer instead of lookup

--- day7/poker.c
+++ day7/poker.c
@@ -4,24 +4,25 @@
 
 struct card {
 	int rank;
-	int suit_lookup;
+	char *suit;
 };
-
-char *suit_names[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
 void print_card(struct card);
 
 int main(void)
 {
+	char d_array[] = {'D', 'i', 'a', 'm', 'o', 'n', 'd', 's', '\0'};
 	struct card three_of_hearts;
 
 	three_of_hearts.rank = 3;
-	three_of_hearts.suit_lookup = 2;
+	three_of_hearts.suit = d_array;
+	print_card(three_of_hearts);
 
+	three_of_hearts.suit = "Hearts";
 	print_card(three_of_hearts);
 }
 
 void print_card(struct card the_card)
 {
-	printf("%d of %s\n", the_card.rank, suit_names[the_card.suit_lookup]);
+	printf("%d of %s\n", the_card.rank, the_card.suit);
 }
