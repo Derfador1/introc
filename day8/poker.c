26d7eb44c95c9579c80c16b141e04617cf3c5482
Liam Echlin <wechlin@umbctraining.com>
2015-10-29 12:55:42 -0400

    Add const flag that may not be modified through its variable

--- day8/poker.c
+++ day8/poker.c
@@ -8,6 +8,13 @@ int main(void)
 {
 	struct deck bicycle;
 
+	const int fievel = 3;
+
+	printf("fievel: %d\n", fievel);
+	// FAILS due to fievel being const
+	fievel = 4;
+
+
 	bicycle.cards[0].rank = 3;
 	bicycle.cards[0].suit = HEARTS;
 	print_card(bicycle.cards[0]);
